96e0083dd3535d3f9c2a0e58365e57d33204b3b3
Nicolas Perejon <nicolas.amorim@shakersagencia.com.br>
2023-05-16 14:04:53 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/**/*.liquid',
+    './sections/**/*.liquid',
+    './snippets/**/*.liquid',
+    './templates/**/*.liquid',
+    './assets/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
